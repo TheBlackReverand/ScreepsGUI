c82dcedceaf776fb4c35c2da3c91d14b726c04ed
Unknown <theblackreverand@gmail.com>
2017-07-04 11:39:43 +0200

    Si le moteur WPF léve une exception, on break pour le signaler

--- ScreepsGUI/App.xaml.cs
+++ ScreepsGUI/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.Windows;
 
 namespace ScreepsGUI
 {
@@ -7,5 +8,25 @@ namespace ScreepsGUI
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            PresentationTraceSources.Refresh();
+            PresentationTraceSources.DataBindingSource.Listeners.Add(new ConsoleTraceListener());
+            PresentationTraceSources.DataBindingSource.Listeners.Add(new DebugTraceListener());
+            PresentationTraceSources.DataBindingSource.Switch.Level = SourceLevels.Warning | SourceLevels.Error;
+            base.OnStartup(e);
+        }
+    }
+
+    public class DebugTraceListener : TraceListener
+    {
+        public override void Write(string message)
+        {
+        }
+
+        public override void WriteLine(string message)
+        {
+            Debugger.Break();
+        }
     }
 }
