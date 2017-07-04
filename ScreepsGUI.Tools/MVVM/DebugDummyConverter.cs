1a012fed19289009367af02554dee08ce8a70bc0
Unknown <theblackreverand@gmail.com>
2017-07-04 11:30:31 +0200

    Ajout d'outil permettant le debugage des DataBinding

---
+++ ScreepsGUI.Tools/MVVM/DebugDummyConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Data;
+
+namespace ScreepsGUI.Tools.MVVM
+{
+    /// <summary>
+    ///     xmlns:diag="clr-namespace:System.Diagnostics;assembly=WindowsBase"
+    ///     
+    ///     <TextBlock Text="{Binding Title, diag:PresentationTraceSources.TraceLevel=High}" />
+    ///     
+    ///     
+    ///     
+    ///     <Window.Resources>
+    ///             <self:DebugDummyConverter x:Key="DebugDummyConverter" />
+    ///     </Window.Resources>
+    ///     
+    ///     <TextBlock Text="{Binding Title, ElementName=wnd, Converter={StaticResource DebugDummyConverter}}" />
+    /// </summary>
+    public class DebugDummyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            Debugger.Break();
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            Debugger.Break();
+            return value;
+        }
+    }
+}
